7aaded9fff449aefe641c431d91f4bee7f6c9098
Laura Nagy <ngylaura@gmail.com>
2018-10-31 09:43:06 +0000

    adds all movies

--- src/components/MovieSelector.js
+++ src/components/MovieSelector.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const MovieSelector = (props) => {
-  console.log(props);
   const options = props.movies.map( (movie, index) => {
     return <option value={index} key={index}>{movie.name}</option>
   });
+
+  const listItems = props.movies.map( (movie) => {
+    return <li>{movie.name}</li>
+  })
 
   function handleChange(event) {
     props.onMovieSelected(event.target.value);
   }
 
   return (
-    <select id="movie-selector" defaultValue="default" onChange={handleChange}>
-      <option disabled value="default">Select a movie to find more about it</option>
-      {options}
-    </select>
+    <Fragment>
+      <select id="movie-selector" defaultValue="default" onChange={handleChange}>
+        <option disabled value="default">Select a movie to find more about it</option>
+        {options}
+      </select>
+      <ol>
+        {listItems}
+      </ol>
+  </Fragment>
   )
 }
 
